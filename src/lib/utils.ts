fc875006cafff1e908bd1c5a9a59756b0e746b88
chukumela <chukumelawilson@gmail.com>
2025-04-27 07:07:54 +0100

    test deploy 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function handleError(error: unknown): string {
+
+  let message = "An unknown error occurred";
+
+  if (typeof error === "string") {
+    message = JSON.stringify(error);
+  }
+
+  return message;
 }
